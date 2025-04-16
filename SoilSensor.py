bd6edeb493c1cc5bb8df19530dd4ee5fd7c2bf43
wwdd <202283890005@nuist.edu.cn>
2025-04-15 15:39:23 -0900

    creat pyhton script SoilSensor.py

---
+++ SoilSensor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+#nuistID:202283890005
+#SetuID:20110009
+#Di Wu
+
+#GPIO SETUP
+channel = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback(channel):
+    if GPIO.input(channel):
+        print ("Water Detected!")
+    else:
+        print ("Water Detected!")
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback) # assign function to GPIO PIN, Run function on change
+
+# infinite loop
+while True:
+    time.sleep(0)
